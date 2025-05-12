4bda0c8439d2ac6e889e53a4b8d08ed626a77a28
kamranhossaintopu <hossainarman953@gmail.com>
2025-05-12 16:06:32 +0600

    Refactored category

--- src/modules/admin/sevice-management/category/dto/create-category.ts
+++ src/modules/admin/sevice-management/category/dto/create-category.ts
@@ -1,21 +1,25 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @ApiProperty({
     example: 'new media',
     description: 'The display name of the category',
   })
+    @IsString()
   name: string;
 
   @ApiProperty({
     example: 'new-media',
     description: 'Slug (URL-friendly identifier) for the category',
   })
+    @IsString()
   slug: string;
 
   @ApiPropertyOptional({
     example: 1,
     description: 'Status of the category (1 = active, 0 = inactive)',
   })
+    @IsNumber()
   status?: number;
 }
