727c3f6b612cc2d8bd06ec5a4a164f41f2eec19d
kamranhossaintopu <hossainarman953@gmail.com>
2025-06-21 10:58:56 +0600

    "website info updated"

--- src/modules/admin/admin_settings/website-info/website-info.service.ts
+++ src/modules/admin/admin_settings/website-info/website-info.service.ts
@@ -153,4 +153,160 @@ export class WebsiteInfoService {
       );
     }
   }
+
+
+  //extra methods can be added here as needed
+  //task insite
+   async getTaskInside() {
+    try {
+      const task = await this.prisma.taskAssign.findMany({
+        orderBy: {
+          created_at: 'desc',
+        },
+      });
+
+      if (!task.length) {
+        return { message: 'No tasks found', tasks: [] };
+      }
+
+      return { message: 'Tasks fetched successfully', 
+        data:{
+          total_tasks: task.length,
+          in_progress_tasks: task.filter(t => t.status === 'In_progress').length,
+          completed_tasks: task.filter(t => t.status === 'completed').length,
+          pending_tasks: task.filter(t => t.status === 'pending').length,
+          pending_review: task.filter(t => t.status === 'pending_review').length,
+
+        }
+       };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        error.message || 'Failed to fetch tasks',
+      );
+    } 
+}
+
+//top performing services
+async getTopPerformingServices() {
+  try {
+    const orders = await this.prisma.order.findMany({
+      select: {
+        pakage_name: true,
+        ammount: true,
+      },
+    });
+
+
+    const packageStats: Record<string, { total_sold: number; total_amount: number }> = {};
+
+    for (const order of orders) {
+      const name = order.pakage_name;
+      const amount = order.ammount;
+
+      if (!packageStats[name]) {
+        packageStats[name] = { total_sold: 0, total_amount: 0 };
+      }
+
+      packageStats[name].total_sold += 1;
+      packageStats[name].total_amount += amount;
+    }
+
+ 
+    const result = Object.entries(packageStats).map(([name, stats]) => ({
+      package_name: name,
+      total_sold: stats.total_sold,
+      total_amount: stats.total_amount,
+    }));
+
+    return {
+      success: true,
+      data: result,
+    };
+  } catch (error) {
+    console.error('Error retrieving orders:', error);
+    throw error;
+  }
+}
+
+//top resellera
+async getTopResellers() {
+  try {
+    const resellers = await this.prisma.reseller.findMany({
+      include: {
+        user: true,
+        reseller_application: true,
+        TaskAssign: true,
+      },
+    });
+
+    const data = resellers.map((reseller) => ({
+      id: reseller.reseller_id,
+      full_name: reseller.full_name,
+      user_email: reseller.user_email,
+      total_earnings: reseller.total_earnings || 0,
+    }));
+
+    //------------Sort by total_earnings in descending order-----------
+    const sortedData = data.sort((a, b) => b.total_earnings - a.total_earnings);
+
+    //-------------Pick top 4 resellers--------------
+    const topResellers = sortedData.slice(0, 4);
+
+    return {
+      success: true,
+      message: 'Top Resellers fetched successfully',
+      data: topResellers,
+    };
+  } catch (error) {
+    console.error('Error fetching resellers:', error);
+    return {
+      success: false,
+      message: `Error fetching resellers: ${error.message}`,
+    };
+  }
+}
+
+//new Orders
+async getRecentOrders() {
+  try {
+    const recentOrders = await this.prisma.order.findMany({
+      take: 5, 
+      orderBy: {
+        created_at: 'desc', 
+      },
+      select: {
+        id: true,
+        created_at: true,
+        order_status: true,
+        subscription_id: true,
+        ammount: true,
+        pakage_name: true,
+        user_email: true,
+        user_name: true,
+        user_id: true,
+        payment_status: true,
+        subscription: {
+          select: {
+            service_id: true,
+            service_tier_id: true,
+          },
+        },
+      },
+    });
+
+    return {
+      success: true,
+      message: "Latest 5 orders fetched successfully",
+      data: recentOrders,
+    };
+  } catch (error) {
+    console.error('Error retrieving recent orders:', error);
+    return {
+      success: false,
+      message: `Error fetching recent orders: ${error.message}`,
+    };
+  }
+}
+
+
 }
